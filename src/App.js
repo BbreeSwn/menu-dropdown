7a8f337685b8af35d142fd9b88b60b37ba6cd09b
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-10 16:28:41 +0700

    feat : set state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Dropdown from './components/Dropdown';
 import Food from './components/Food';
+import MenuData from './data/MenuData';
+import { useState } from 'react';
 
 function App() {
+const [food , setFood] = useState(MenuData)
+
   return (
     <div className="container">
       <Dropdown />
-      <Food />
+      <Food food={food} setFood={setFood} />
     </div>
   );
 }
